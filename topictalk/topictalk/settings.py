2b8bc88c0bba28b12cfbaa26718f276ac83f04eb
Iliya Raev <131691288+IliyaSR@users.noreply.github.com>
2024-01-31 18:54:01 +0200

    Add files via upload

--- topictalk/topictalk/settings.py
+++ topictalk/topictalk/settings.py
@@ -136,4 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = ''  # your host email
+EMAIL_HOST_PASSWORD = ''  # the password is an automatically generated password by Gmail
